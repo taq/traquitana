db95f4e12526fbf18711a95c6bec95a30cc4f8cf
Eustaquio Rangel <taq@eustaquiorangel.com>
2012-08-23 08:40:33 -0300

    Fixed selector test.
    Now there is an asset file. Needed to increment the test result.

--- spec/selector_spec.rb
+++ spec/selector_spec.rb
@@ -9,6 +9,6 @@ describe Traquitana::Selector do
 
    it "should return the file list" do
       list = Traquitana::Selector.new(File.expand_path(File.dirname(__FILE__))+"/config/").files
-      list.size.must_equal 23
+      list.size.must_equal 24
    end
 end
